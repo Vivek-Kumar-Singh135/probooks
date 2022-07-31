429128a43b2bd051374ed6889d5c038830731810
Vivek-Kumar-Singh135 <vivekkrsingh135@gmail.com>
2022-07-31 12:34:24 +0530

    Updates

---
+++ precache-manifest.35bbd3c44570500e0e21875e259cb367.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c6576f54aca2e9dcd98",
+    "url": "/static/css/main.844d382b.chunk.css"
+  },
+  {
+    "revision": "0c6576f54aca2e9dcd98",
+    "url": "/static/js/main.0c6576f5.chunk.js"
+  },
+  {
+    "revision": "02858a3d24d3e137e095",
+    "url": "/static/js/1.02858a3d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "d44501400af9950aa0c77dab9a14fea4",
+    "url": "/index.html"
+  }
+];
